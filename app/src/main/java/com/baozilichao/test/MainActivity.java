2ab97c4f1d848f292070dba9d5a3c515e0d96331
baozilichao <baozilichao@126.com>
2016-11-08 16:08:36 +0800

    time1

--- app/src/main/java/com/baozilichao/test/MainActivity.java
+++ app/src/main/java/com/baozilichao/test/MainActivity.java
@@ -1,11 +1,14 @@
 package com.baozilichao.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        v.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
         Log.e("dgdfg", "initView: " + 111111);
     }
 
